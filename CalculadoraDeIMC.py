90c32752af4254afc7f68c954af4ca16e7dea50a
Milena Sviatek <81256013+milasvi@users.noreply.github.com>
2022-05-09 16:08:34 -0300

    Update CalculadoraDeIMC.py

--- CalculadoraDeIMC.py
+++ CalculadoraDeIMC.py
@@ -1,11 +1,12 @@
 #condicional aninhada
-print('Bem-vinde à Calculadora de IMC para Adultos! (20 a 60 anos')
+print('Bem-vinde à Calculadora de IMC para Adultos! (20 a 60 anos)')
 #Menor que 18.5 - Abaixo do peso ;
 #Entre 18.5 e 24.9 - Peso normal ;
 # Entre 25.0 e 29.9 - Pré-obesidade ;
 # Entre 30.0 e 40 - Obesidade 
+
 while True:
-    altura = float(input('Qual sua altura?'))
+    altura = float(input('Qual sua altura? (Digite com ponto em vez de vírgula)'))
     peso = float(input('Qual seu peso?'))
     imc = peso / (altura ** 2)
 
@@ -14,11 +15,11 @@ while True:
     if imc < 18.6:
         print('Seu IMC está abaixo do normal, procure um(a) médico(a)!')
     elif imc == 18.5 or imc < 24.9 or imc == 24.9:
-        print('Seu IMC está normal, mas procure um médico para fazer um checkup mesmo assim.')
+        print('Seu IMC está normal, mas procure um(a) médico(a) para fazer um checkup mesmo assim.')
     elif imc > 25 or imc == 25 or imc < 29.9 or imc == 29.9:
         print('Seu IMC está sobrepeso, procure um(a) médico(a) para fazer um checkup!)')
     else:
-        print('Seu IMC é obesidade, procure um(a) médico(a) para fazer um checkup.')
+        print('Seu IMC é obeso, procure um(a) médico(a) para fazer um checkup.')
     resposta = input('Deseja informar outro IMC? [s/n]')
     if resposta == 'n':
         print('Obrigade por usar a Calculadora de IMC. \nAté mais!')
